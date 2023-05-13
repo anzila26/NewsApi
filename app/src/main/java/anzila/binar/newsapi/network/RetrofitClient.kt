fd454ee3eeb0bad4f336f4a5f3393f95ef53d1d9
anzila26 <sitianzilanur@gmail.com>
2023-05-13 14:38:57 +0700

    Initial commit

---
+++ app/src/main/java/anzila/binar/newsapi/network/RetrofitClient.kt
@@ -0,0 +1,29 @@
+package anzila.binar.newsapi.network
+
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object RetrofitClient {
+    const val BASE_URL = "https://newsapi.org/v2/"
+
+    private  val logging : HttpLoggingInterceptor
+        get(){
+            val httpLoggingInterceptor = HttpLoggingInterceptor()
+            return httpLoggingInterceptor.apply {
+                httpLoggingInterceptor.level = HttpLoggingInterceptor.Level.BODY
+            }
+        }
+
+    private val clint = OkHttpClient.Builder().addInterceptor(logging).build()
+
+    val instance : RestfulApi by lazy {
+        val retrofit = Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .client(clint)
+            .build()
+        retrofit.create(RestfulApi::class.java)
+    }
+}
---
+++ app/src/main/java/anzila/binar/newsapi/view/MainActivity.kt
@@ -0,0 +1,24 @@
+package anzila.binar.newsapi.view
+
+import android.content.Context
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.view.ViewGroup
+import android.widget.Button
+import anzila.binar.newsapi.R
+import anzila.binar.newsapi.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    lateinit var binding : ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        Handler().postDelayed({
+            startActivity((Intent(this, CategoryActivity::class.java)))
+        },3000)
+    }
+}
